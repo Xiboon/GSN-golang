05ca66b99dfc2c8d6d1f9c7a8c2f676bfd260770
Xiboon <38988733+Xiboon@users.noreply.github.com>
2020-08-26 17:37:00 +0200

    tat

---
+++ gsn.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"strings"
+)
+
+
+func main() {
+	content, err := ioutil.ReadFile("./adjectives.txt")
+	if err != nil {
+
+		//Do something
+	}
+	lines := strings.Split(string(content), "\n")
+	content2, err := ioutil.ReadFile("./nouns.txt")
+	if err != nil {
+		//Do something
+	}
+	lines2 := strings.Split(string(content2), "\n")
+	fmt.Printf("Czy chcesz liczbę w swoim nicku?")
+	var input string
+	fmt.Scanf("%s", &input)
+	fmt.Printf("test:" + input)
+ if input == "y" {
+
+	 fmt.Printf(lines[rand.Intn(308)] + "_" + lines2[rand.Intn(308)])
+
+ } else if input == "n" {
+	 fmt.Printf("Wpisałeś n!")
+} }
